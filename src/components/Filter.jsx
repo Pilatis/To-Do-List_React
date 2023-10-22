2892367db8968a79fc820f647ce248c46fa33173
Pilatis <pilatilucas@gmail.com>
2023-10-21 21:17:48 -0300

    first commit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Filter = ({ filter, setFilter, setSort }) => {
+  return (
+    <div className='filter'>
+        <h2>Filtrar: </h2>
+        <div className="filter-options">
+            <div>
+                <p>Status: </p>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                   <option value="All">Todas</option> 
+                   <option value="Completed">Completas</option>
+                   <option value="Incomplete">Incompletas</option>
+                </select>
+            </div>
+              <div>
+                <p>Ordem alfabética:</p>
+                <button onClick={() => setSort("Crecente")}>Crecente</button>
+                <button onClick={() => setSort("Decrecente")}>Decrecente</button>
+              </div>
+        </div>
+    </div>
+  )
+}
+
+export default Filter
